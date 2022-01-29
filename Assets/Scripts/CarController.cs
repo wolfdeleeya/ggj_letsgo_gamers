b7f20062d8e26dfe0647e705a8921529deb81d1c
FrankoGracin <frankogracin17@gmail.com>
2022-01-29 15:28:13 +0100

    auto radi s mobom

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CarController : MonoBehaviour
 {
@@ -28,6 +29,8 @@ public class CarController : MonoBehaviour
     private float _maxScale;
     [SerializeField]
     private float _minScale;
+    bool isPhoneView;
+    bool wheelHold;
 
     // Start is called before the first frame update
     void Start()
@@ -35,80 +38,94 @@ public class CarController : MonoBehaviour
         rb = this.GetComponent<Rigidbody>();
         trans = transform;
         acamera = this.GetComponentInChildren<Camera>();
+        isPhoneView = false;
+        wheelHold = false;
         
     }
     private void Update()
     {
+        
+
+        //find out center of Wheel in screen width and length
         centerWheelScreen = acamera.WorldToScreenPoint(wheelTrans.position);
-        if (Input.GetMouseButtonDown(0))
+
+        //when click mouse
+        if (Input.GetMouseButtonDown(0) && !isPhoneView)
         {
             Vector2 mousePos = Input.mousePosition;
             Vector3 worldPosition = acamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, acamera.nearClipPlane));
             Vector3 smjer = (worldPosition - cam.transform.position).normalized * 100;
-            //Debug.Log(worldPosition);
-            if (Physics.Raycast(cam.transform.position, smjer, out RaycastHit hit1, wheelMask))
+            
+            //if user clicked on steering wheel
+            if (Physics.Raycast(cam.transform.position, smjer, out RaycastHit hit1))
             {
-                vecStart = (mousePos- centerWheelScreen).normalized;
+                if (hit1.collider.gameObject.name.Equals("steering_wheel"))
+                {
+                    vecStart = (mousePos - centerWheelScreen).normalized;
+                }
+                    //remember vector position from center to the point the user clicked no steering wheel
+               
 
+            }
+            else
+            {
+                vecStart = Vector3.zero;
             }
            
         }
 
-        if (Input.GetMouseButton(0))
+        //if user holding the mouse button on steering wheel
+        //realAngle: true angle from wheel origin position to current position of wheel
+        if (Input.GetMouseButton(0) && vecStart.magnitude != 0 && !isPhoneView)
         {
+            wheelHold = true;
+            
+            //calculate vector direction from the camera position to the mouse hold position
             Vector2 mousePos = Input.mousePosition;
             Vector3 worldPosition = acamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, acamera.nearClipPlane));
             Vector3 smjer = (worldPosition - cam.transform.position).normalized * 100;
-            //Debug.Log(worldPosition);
-            if (Physics.Raycast(cam.transform.position, smjer, out RaycastHit hit1, wheelMask))
+            
+            //if this direction is passing throgh the wheel box collider, user is holding the wheel 
+            if (Physics.Raycast(cam.transform.position, smjer, out RaycastHit hit1))
             {
-                Vector3 vec = (mousePos - centerWheelScreen).normalized;
-                //Debug.Log(vec);
-                float cross = Mathf.Sign(Vector3.Cross(vecStart, vec).z);
-                float angle = Vector3.Angle(vecStart, vec);
-                if (Mathf.Abs(realAngle) > maxAngle && Mathf.Abs(realAngle - cross) > Mathf.Abs(realAngle))
+                if (hit1.collider.gameObject.name.Equals("steering_wheel"))
                 {
+                    Debug.Log("wheelCast" + hit1.collider.gameObject);
+                    Vector3 vec = (mousePos - centerWheelScreen).normalized;
 
+                    float cross = Mathf.Sign(Vector3.Cross(vecStart, vec).z);
+                    float angle = Vector3.Angle(vecStart, vec);
+                    if (Mathf.Abs(realAngle) > maxAngle && Mathf.Abs(realAngle - cross) > Mathf.Abs(realAngle))
+                    {
+
+                    }
+                    else
+                    {
+
+                        wheelTrans.RotateAround(wheelTrans.position, wheelTrans.up, angle * -cross);
+                        vecStart = vec;
+                        //turn Angle
+
+                        realAngle += angle * -cross;
+
+                        Quaternion target = Quaternion.Euler(0, realAngle * turnScale, 0);
+
+
+                        trans.rotation = Quaternion.Slerp(trans.rotation, target, Time.deltaTime * smoothTurn);
+                    }
                 }
-                else
-                {
-                    //Debug.Log(res);
-                    wheelTrans.RotateAround(wheelTrans.position, wheelTrans.up, angle * -cross);
-                    vecStart = vec;
-                    //turn Angle
-
-                    realAngle += angle * -cross ;
-
-                    Quaternion target = Quaternion.Euler(0, realAngle * turnScale, 0);
-
-                    // Dampen towards the target rotation
-                    trans.rotation = Quaternion.Slerp(trans.rotation, target, Time.deltaTime * smoothTurn);
-                }
+                
+            }
+            else
+            {
+                wheelHold = false;
             }
 
 
         }
         else
         {
-            if (Mathf.Abs(realAngle) > 5)
-            {
-
-                wheelTrans.RotateAround(wheelTrans.position, wheelTrans.up,  servoSpeed * -Mathf.Sign(realAngle));
-                realAngle += servoSpeed *- Mathf.Sign(realAngle);
-                Quaternion target = Quaternion.Euler(0, realAngle*turnScale, 0);
-
-                // Dampen towards the target rotation
-                trans.rotation = Quaternion.Slerp(trans.rotation, target, Time.deltaTime * smoothTurn);
-
-            }
-            else if(Mathf.Abs(realAngle) != 0)
-            {
-                wheelTrans.RotateAround(wheelTrans.position, wheelTrans.up, realAngle * -Mathf.Sign(realAngle));
-                realAngle = 0;
-
-                StartCoroutine(BumpAnimation());
-                
-            }
+            ActivateServo();
         }
     }
 
@@ -135,5 +152,37 @@ public class CarController : MonoBehaviour
             trans.rotation = Quaternion.Euler(0, angle * sign, 0);
         }
     }
+    public void switchView()
+    {
+        isPhoneView = !isPhoneView;
+    }
 
+    private void ActivateServo()
+    {
+        if (Mathf.Abs(realAngle) > 5)
+        {
+
+            reverseRotateWheel();
+            
+            realAngle += servoSpeed * -Mathf.Sign(realAngle);
+
+            Quaternion target = Quaternion.Euler(0, realAngle * turnScale, 0);
+
+            trans.rotation = Quaternion.Slerp(trans.rotation, target, Time.deltaTime * smoothTurn);
+
+        }
+        else if (Mathf.Abs(realAngle) != 0)
+        {
+            reverseRotateWheel();
+            realAngle = 0;
+
+            StartCoroutine(BumpAnimation());
+
+        }
+    }
+
+    private void reverseRotateWheel()
+    {
+        wheelTrans.RotateAround(wheelTrans.position, wheelTrans.up, servoSpeed * -Mathf.Sign(realAngle));
+    }
 }
